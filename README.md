e623fbdf44c70ae1bba02bf6e1a7d810eed16f5c
Johannes Hofmeister <github@account.cessor.de>
2018-06-08 10:36:06 +0200

    Made the program unix ready

---
+++ README.md
@@ -0,0 +1,4 @@
+blockhosts
+==========
+
+It blocks hosts.
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='blockhosts',
+      version='1.0',
+      description='Blocks Hosts',
+      author='Johannes Hofmeister',
+      author_email='py_blockhosts@spam.cessor.de',
+      url='https://github.com/cessor/blockhosts/',
+      packages=['blockhosts'],
+      scripts=["blockhosts.sh", "blockhosts.bat"]
+     )
